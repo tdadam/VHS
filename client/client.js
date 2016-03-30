fef990fecc66a84c5a6e52f2071c596b75c7b1c8
Tim Adam <tdadam@xactware.com>
2016-03-30 09:59:08 -0600

    front end

--- client/client.js
+++ client/client.js
@@ -1,16 +1,48 @@
 angular.module('app', ['ngMaterial'])
 
-.component('main', {
-	templateUrl: 'main.html',
-	controller: MainCtrl,
-	bindings: {
-	}
-});
+	.component('main', {
+		templateUrl: 'main.html',
+		controller: MainCtrl,
+		bindings: {
+		}
+	});
 
 function MainCtrl($http) {
-	this.greeting = 'hi';
+	this.$http = $http;
+	this.greeting = 'VHS Store';
+	this.movies = [
+		{
+			title: 'Movie Title 1',
+			year: 1988,
+			checkedOut: false
+		}
+	];
 }
 
 MainCtrl.prototype.sayHi = function() {
 	console.log(this.greeting);
-}
+};
+
+MainCtrl.prototype.checkOut = function(movie) {
+	console.log('checkout', movie);
+};
+
+MainCtrl.prototype.removeMovie = function(movie) {
+	console.log('remove', movie);
+};
+
+MainCtrl.prototype.search = function(text) {
+	if (text === undefined || text === '') {
+		console.log('list all movies');
+		this.$http.get('/api/movies').then(response => {
+			console.log('api movies', response.data);
+			this.movies = response.data;
+		});
+	} else {
+		console.log('search for movies:', text);
+	}
+};
+
+MainCtrl.prototype.saveMovie = function(title, year) {
+	console.log('save movie', title, year);
+};
--- server.js
+++ server.js
@@ -2,11 +2,28 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var movies = [
+	{
+		title: 'Movie Title 1',
+		year: 1988,
+		checkedOut: true
+	},
+	{
+		title: 'Movie Title 2',
+		year: 1989,
+		checkedOut: false
+	}
+];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', express.static(__dirname + '/client'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.get('/api/movies', (req, res) => {
+	res.json(movies).end();
+});
 
 var port = 3343;
 app.listen(port, function() {
