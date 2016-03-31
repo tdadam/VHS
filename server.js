9569c363443c4a7225e5c80a7f2246657284a8ba
Tim Adam <tdadam@xactware.com>
2016-03-31 10:08:21 -0600

    good port

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/api/movies', (req, res) => {
 	res.json(movies).end();
 });
 
-var port = 3343;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log(`App listening on port ${port}...`);
 });
